3666483484b3c4b4666766bed68997b224c150a6
Krzysztof Wasik <krzysztof.wasik@auto.com.pl>
2017-12-08 08:55:22 +0100

    AES_SHA256 with openssl

---
+++ Tests/CryptoSystem/AES_SHA256Test.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\CryptoSystem;
+
+use Rosio\EncryptedCookie\CryptoSystem\AES_SHA256;
+
+class AES_SHA256Test extends \PHPUnit_Framework_TestCase
+{
+	protected $cryptoSystem;
+
+	function setUp ()
+	{
+		$this->cryptoSystem = new AES_SHA256($this->getRandom(32), "SomeHMAC");
+	}
+
+	public function testEncryptDecrypt ()
+	{
+		$data = 'Some data.';
+
+		$encData = $this->cryptoSystem->encrypt($data, 0);
+		$decData = $this->cryptoSystem->decrypt($encData);
+
+		$this->assertEquals($data, $decData);
+
+		$data = 'Some data. Special characters: ' . chr(1) . chr(2) . chr(3) . '.';
+
+		$encData = $this->cryptoSystem->encrypt($data, 0);
+		$decData = $this->cryptoSystem->decrypt($encData);
+
+		$this->assertEquals($data, $decData);
+	}
+
+	public function testConstantTimeComparator ()
+	{
+		$this->assertTrue(AES_SHA256::ctComp('', ''));
+		$this->assertTrue(AES_SHA256::ctComp('data', 'data'));
+
+		$this->assertFalse(AES_SHA256::ctComp('', 'data'));
+		$this->assertFalse(AES_SHA256::ctComp('data', ''));
+
+		$this->assertFalse(AES_SHA256::ctComp('data', 'as'));
+		$this->assertFalse(AES_SHA256::ctComp('as', 'data'));
+
+		$this->assertFalse(AES_SHA256::ctComp(chr(0), chr(1)));
+		$this->assertTrue(AES_SHA256::ctComp(chr(1), chr(1)));
+	}
+
+	/**
+	 * @expectedException \Rosio\EncryptedCookie\Exception\InputExpiredException
+	 */
+	public function testExpiration ()
+	{
+		$data = 'Some data.';
+
+		$encData = $this->cryptoSystem->encrypt($data, 1);
+		sleep(2);
+		$decData = $this->cryptoSystem->decrypt($encData);
+	}
+
+	public function testOkayExpiration ()
+	{
+		$data = 'Some data.';
+
+		$encData = $this->cryptoSystem->encrypt($data, 2);
+		$decData = $this->cryptoSystem->decrypt($encData);
+
+		$this->assertEquals($data, $decData);
+	}
+
+	/**
+	 * @expectedException \Rosio\EncryptedCookie\Exception\TIDMismatchException
+	 */
+	public function testTIDMismatch ()
+	{
+		$data = 'Some data.';
+
+		$encData = $this->cryptoSystem->encrypt($data, 2);
+		$this->setUp(); // Force new keys
+		$decData = $this->cryptoSystem->decrypt($encData);
+	}
+
+	/**
+	 * @expectedException \Rosio\EncryptedCookie\Exception\InputTamperedException
+	 */
+	public function testInputTampered ()
+	{
+		$data = 'Some data.';
+
+		$encData = $this->cryptoSystem->encrypt($data, 2);
+
+		$badEncData = substr($encData, 0, -4); // Do 4 to make sure we don't get base64's padding
+		$this->assertNotEquals($encData, $badEncData);
+		
+		$decData = $this->cryptoSystem->decrypt($badEncData);
+	}
+
+	protected function getRandom ($length)
+	{
+		$str = '';
+		$letters = range('a', 'z');
+
+		for ($i = 0; $i < $length; $i++)
+			$str .= $letters[rand(0, count($letters) - 1)];
+
+		return $str;
+	}
+}
---
+++ src/CryptoSystem/AES_SHA256.php
@@ -0,0 +1,96 @@
+<?php
+namespace Rosio\EncryptedCookie\CryptoSystem;
+
+use Rosio\EncryptedCookie\Exception\RNGUnavailableException;
+use Rosio\EncryptedCookie\Exception\InputTamperedException;
+use Rosio\EncryptedCookie\Exception\InputExpiredException;
+use Rosio\EncryptedCookie\Exception\TIDMismatchException;
+
+class AES_SHA256 implements iCryptoSystem
+{
+    const IV_SIZE = 16;
+
+    protected $symmetricKey;
+    protected $HMACKey;
+
+    public function __construct($symmetricKey, $HMACKey)
+    {
+        if (strlen($symmetricKey) != 32)
+            throw new \InvalidArgumentException('Symmetric key must be 32 bytes/characters long.');
+
+        $this->symmetricKey = $symmetricKey;
+        $this->HMACKey = $HMACKey;
+    }
+
+    public function encrypt($data, $expiration)
+    {
+        $iv = $this->getRandom(self::IV_SIZE);
+        $atime = time();
+        $tid = $this->getTID();
+
+        $encData = openssl_encrypt($data, 'AES-256-CBC', $this->symmetricKey, null, $iv);
+
+        $hmac = $this->getHMAC($encData, $atime, $expiration, $tid, $iv);
+
+        return base64_encode($encData) . '|' . base64_encode($atime) . '|' . base64_encode($expiration) . '|' . base64_encode($tid) . '|' . base64_encode($iv) . '|' . base64_encode($hmac);
+    }
+
+    public function decrypt($data)
+    {
+        list($encData, $atime, $expiration, $tid, $iv, $hmac) = array_map('base64_decode', explode('|', $data));
+
+        if (!self::ctComp($tid, $this->getTID()))
+            throw new TIDMismatchException('The data TID no longer matches the crypto system TID.');
+
+        $generatedHMAC = $this->getHMAC($encData, $atime, $expiration, $tid, $iv);
+
+        if (!self::ctComp($hmac, $generatedHMAC))
+            throw new InputTamperedException('The data HMAC no longer matches.');
+
+        if ($expiration > 0 && $atime + $expiration < time())
+            throw new InputExpiredException('The expiration time on the data has been reached.');
+
+        $data = trim(openssl_decrypt($encData, 'AES-256-CBC', $this->symmetricKey, null,$iv));
+
+        return $data;
+    }
+
+    protected function getRandom($length)
+    {
+        $wasCryptoSecure = false;
+        $random = openssl_random_pseudo_bytes($length, $wasCryptoSecure);
+
+        if ($wasCryptoSecure !== true)
+            throw new RNGUnavailableException('The RNG was unable to provide truely random numbers.');
+
+        return $random;
+    }
+
+    static function ctComp($value1, $value2)
+    {
+        $differences = 0;
+        for ($i = 0; $i < max(strlen($value1), strlen($value2)); $i++)
+        {
+            $v1 = isset($value1[$i]) ? ord($value1[$i]) : -1;
+            $v2 = isset($value2[$i]) ? ord($value2[$i]) : -1;
+
+            $differences |= $v1 ^ $v2;
+        }
+        return $differences === 0;
+    }
+
+    protected function getHMAC($encryptedData, $aTime, $expiration, $tid, $iv)
+    {
+        return hash_hmac('sha256', base64_encode($encryptedData) . base64_encode($aTime) . base64_encode($expiration) . base64_encode($tid) . base64_encode($iv), $this->HMACKey, true);
+    }
+
+    /**
+     * Get a string which uniquely represents the algorithms and keys used to encrypt the data.
+     * 
+     * @return string
+     */
+    function getTID()
+    {
+        return substr(md5(md5($this->symmetricKey) . 'AES_SHA256' . md5($this->HMACKey)), 0, 8);
+    }
+}
