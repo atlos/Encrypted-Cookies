4de6d1e65d1206725546bc8ffecc0f85bf8b19e1
Krzysztof Wasik <krzysztof.wasik@auto.com.pl>
2018-01-09 07:54:52 +0100

    PHPUnit comments

--- src/StorageSystem/CookieArrayStorageSystem.php
+++ src/StorageSystem/CookieArrayStorageSystem.php
@@ -4,19 +4,40 @@ namespace Rosio\EncryptedCookie\StorageSystem;
 
 class CookieArrayStorageSystem implements iStorageSystem
 {
-	public function get ($name)
-	{
-		return $_COOKIE[$name];
-	}
 
-	public function has ($name)
-	{
-		return isset($_COOKIE[$name]);
-	}
+    /**
+     * 
+     * @param string $name
+     * @return string
+     */
+    public function get($name)
+    {
+        return $_COOKIE[$name];
+    }
 
-	public function set ($name, $data, $expiration, $domain, $path, $isSecure, $isHttpOnly)
-	{
+    /**
+     * 
+     * @param string $name
+     * @return bool
+     */
+    public function has($name)
+    {
+        return isset($_COOKIE[$name]);
+    }
+
+    /**
+     * 
+     * @param string $name
+     * @param mixed $data
+     * @param int $expiration
+     * @param string $domain
+     * @param string $path
+     * @param bool $isSecure
+     * @param bool $isHttpOnly
+     */
+    public function set($name, $data, $expiration, $domain, $path, $isSecure, $isHttpOnly)
+    {
         setcookie($name, $data, $expiration, $path, $domain, $isSecure, $isHttpOnly);
         $_COOKIE[$name] = $data;
-	}
+    }
 }
